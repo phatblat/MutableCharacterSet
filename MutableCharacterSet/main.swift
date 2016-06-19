6047b4fdc82b964c2879dbfc8e4698840d2ff7f0
Ben Chatelain <ben@octop.ad>
2016-06-18 22:27:34 -0600

    Change to CharacterSet API

--- MutableCharacterSet/main.swift
+++ MutableCharacterSet/main.swift
@@ -12,7 +12,7 @@ public extension String {
     func urlEncodedString() -> String? {
         let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
         let subDelimitersToEncode = "!$&'()*+,;="
-        var characterSet = NSCharacterSet.urlQueryAllowed()
+        var characterSet = CharacterSet.urlQueryAllowed
         characterSet.remove(charactersIn: generalDelimitersToEncode + subDelimitersToEncode)
         return addingPercentEncoding(withAllowedCharacters: characterSet)
     }
